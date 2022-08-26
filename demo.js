2d6d10bf289edad16baa4343c742a227e68c7ca4
yacel100 <master.yac@gmail.com>
2022-08-25 21:38:50 -0400

    Agregando fecha 25/08/2022 al archivo demo

--- demo.js
+++ demo.js
@@ -1,5 +1,2 @@
-console.log("Hola clase");
-console.log("Diplomado Fullstack");
-
-console.log("Hola clase dia 6");
+console.log("Hola clase dia 6 - 25/08/2022");
 console.log("Diplomado Fullstack");
